373d8c66ed771e48473fcd1910c578e33945604a
rk.dev <rekubota.dev@gmail.com>
2026-09-23 21:13:46 +0900

    refactor: ログインをOAuth2PasswordRequestFormに対応

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from app.database import get_db
 from app.models.user import User
-from app.schemas.auth import LoginRequest, TokenResponse
+from app.schemas.auth import TokenResponse
 from app.security import create_access_token, verify_password
 
 
@@ -12,9 +13,12 @@ router = APIRouter(prefix="/auth", tags=["Auth"])
 
 # POST
 @router.post("/login", response_model=TokenResponse)
-def login(login_data: LoginRequest, db: Session = Depends(get_db)):
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(), 
+    db: Session = Depends(get_db)
+):
     user = db.query(User).filter(
-        User.email == login_data.email
+        User.email == form_data.username
     ).first()
     
     if user is None:
@@ -23,7 +27,7 @@ def login(login_data: LoginRequest, db: Session = Depends(get_db)):
             detail="Invalid email or password"
         )
     
-    if not verify_password(login_data.password, user.password_hash):
+    if not verify_password(form_data.password, user.password_hash):
         raise HTTPException(
             status_code=401, 
             detail="Invalid email or password"
